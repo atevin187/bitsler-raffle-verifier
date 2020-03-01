7a994ddaec43c874e4e61430cd643f8cc168e28a
Kamil Cholewa <atevin187@gmail.com>
2020-03-01 16:41:14 +0100

    Display results in table

--- index.js
+++ index.js
@@ -5,21 +5,35 @@ const rawData = fs.readFileSync('./data.json');
 const jsonData = JSON.parse(rawData);
 
 const blockHash = crypto.createHash('sha256').update(jsonData.block).digest('hex');
-const winningNumber = parseInt(blockHash.slice(0, 7), 16);
+const raffleNumber = parseInt(blockHash.slice(0, 7), 16);
 
-console.log('Winning number: ' + winningNumber + '\n');
+console.log('Raffle Number: ' + raffleNumber + '\n');
 
-for (player of jsonData.players) {
+function playerClosestNumber(player, tickets, number) {
     let closestNumber = 0;
 
-    for (let i = 1; i <= player[1]; i++) {
-        let playerTicketHash = crypto.createHash('sha256').update(player[0] + '-' + i).digest('hex');
+    for (let i = 1; i <= tickets; i++) {
+        let playerTicketHash = crypto.createHash('sha256').update(player + '-' + i).digest('hex');
         let playerTicket = parseInt(playerTicketHash.slice(0, 7), 16);
 
-        if (Math.abs(winningNumber - playerTicket) < Math.abs(winningNumber - closestNumber)) {
+        if (Math.abs(number - playerTicket) < Math.abs(number - closestNumber)) {
             closestNumber = playerTicket;
         }
     }
 
-    console.log(player[0] + ': ' + closestNumber);
+    return closestNumber;
 }
+
+let results = [];
+
+for (player of jsonData.players) {
+    results.push(
+        {
+            "user": player[0],
+            "points": player[1],
+            "closest_ticket": playerClosestNumber(player[0], player[1], raffleNumber)
+        }
+    );
+}
+
+console.table(results);
